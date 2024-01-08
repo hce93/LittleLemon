5a5e2b0509be0cfa6383eca71fef673f4d428d3e
hce93 <henry.evison@Henrys-MacBook-Pro.local>
2024-01-08 12:39:34 +0000

    fourth

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin 
-from django.urls import path 
+from django.urls import path
+from . import views 
 from .views import sayHello 
   
 urlpatterns = [ 
-    path('', sayHello, name='sayHello'), 
+    path('', views.index, name='index'), 
 ]
