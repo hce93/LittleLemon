8afa4393e941a5c2533534592b12c0e58de87477
hce93 <henry.evison@Henrys-MacBook-Pro.local>
2024-01-12 18:17:04 +0000

    tenth branch

--- littlelemon/restaurant/forms.py
+++ littlelemon/restaurant/forms.py
@@ -5,19 +5,27 @@ from .models import Booking
 # Code added for loading form data on the Booking page
 class BookingForm(forms.ModelForm):
     no_of_guests = forms.ChoiceField(
-        choices=[('', 'Select Guests')] + [(i, str(i)) for i in range(1, 9)],
+        choices=[('', 'Select Guests')] + [(i, str(i)) for i in range(1, 7)],
         label='Number of Guests',
         initial='',
         widget=forms.Select(attrs={'class': 'form-control'})
     )
-    
-    reservation_slot = forms.DateTimeField(
-        label='Reservation Slot',
-        widget=forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
-        input_formats=['%Y-%m-%dT%H:%M'],  # Adjust the format based on your needs
+    reservation_date = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date'}),
     )
+    
+    reservation_time = forms.ChoiceField(
+        choices = [(f"{h:02d}:{m:02d}", f"{h:02d}:{m:02d}") for h in range(11, 22) for m in range(0, 60, 30)],
+        label='Reservation Time',
+        widget=forms.Select(attrs={'class': 'form-control'}))
+    
+    # reservation_slot = forms.DateTimeField(
+    #     label='Reservation Slot',
+    #     widget=forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+    #     input_formats=['%Y-%m-%dT%H:%M'],  # Adjust the format based on your needs
+    # )
     
     class Meta:
         model = Booking
-        fields = ['name', 'no_of_guests', 'reservation_slot']
+        fields = ['name', 'no_of_guests', 'reservation_date', 'reservation_time']
         
